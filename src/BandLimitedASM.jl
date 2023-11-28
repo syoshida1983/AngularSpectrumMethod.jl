2e64258f4f08a3d0f7488facc3828c4d522cb455
Shuhei Yoshida <98638350+syoshida1983@users.noreply.github.com>
2023-11-28 22:39:17 +0900

    Update BandLimitedASM.jl

--- src/BandLimitedASM.jl
+++ src/BandLimitedASM.jl
@@ -5,7 +5,7 @@ export BandLimitedASM
 
 return diffracted field by the band-limited ASM (see Ref. 1).
 
-> 1. [Kyoji Matsushima and Tomoyoshi Shimobaba, "Band-Limited Angular Spectrum Method for Numerical Simulation of Free-Space Propagation in Far and Near Fields," Opt. Express **17**, 19662-19673 (2009) ](https://doi.org/10.1364/OE.17.019662)
+> 1. [Kyoji Matsushima and Tomoyoshi Shimobaba, "Band-Limited Angular Spectrum Method for Numerical Simulation of Free-Space Propagation in Far and Near Fields," Opt. Express **17**, 19662-19673 (2009)](https://doi.org/10.1364/OE.17.019662)
 """
 function BandLimitedASM(u, λ, Δx, Δy, z; expand=true)
     û = ifelse(expand, ifftshift(fft(fftshift(padzeros(u)))), ifftshift(fft(fftshift(u))))
