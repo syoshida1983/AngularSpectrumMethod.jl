f1cc8c0ce89da99f2aa298372e41bc862f4da823
Shuhei Yoshida <98638350+syoshida1983@users.noreply.github.com>
2023-11-28 22:40:27 +0900

    Update ShiftedASM.jl

--- src/ShiftedASM.jl
+++ src/ShiftedASM.jl
@@ -27,9 +27,9 @@ end
 """
     ShiftedASM(u, λ, Δx, Δy, z, x₀, y₀; expand=true)
 
-return shifted diffraction field with the shift distance ``x_{0}`` and ``y_{0}`` by the shifted ASM (see Ref. 1).
+return shifted diffraction field with the shift distance ``x_{0}`` and ``y_{0}`` by the shifted ASM (see Ref. 3).
 
-> 1. [Kyoji Matsushima, "Shifted angular spectrum method for off-axis numerical propagation," Opt. Express **18**, 18453-18463 (2010)](https://doi.org/10.1364/OE.18.018453)
+> 3. [Kyoji Matsushima, "Shifted angular spectrum method for off-axis numerical propagation," Opt. Express **18**, 18453-18463 (2010)](https://doi.org/10.1364/OE.18.018453)
 """
 function ShiftedASM(u, λ, Δx, Δy, z, x₀, y₀; expand=true)
     û = ifelse(expand, ifftshift(fft(fftshift(padzeros(u)))), ifftshift(fft(fftshift(u))))
